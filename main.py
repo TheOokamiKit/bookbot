e7f8711594916309b45bf36e15b02bf5e3c06765
TheOokamiKit <tyranime@gmail.com>
2025-02-22 19:11:41 -0600

    #5 now we are counting letters!

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ def main ():
             file_contents = f.read()
             return file_contents
     from stats import word_count
+    from stats import letter_count
 
 
     #provides the filepath to the book to evaluate then prints the word count to console
     contents = get_book_text("books/frankenstein.txt")
     words = word_count(contents)
+    letters = letter_count(contents)
     print(f"{words} words found in the document")
+    print(letters)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def word_count(string):
         words = string.split()
         return len(words)
+def letter_count(string):
+    string = str.lower(string)
+    count = {}
+    for i in string:
+        if i in count:
+            count[i] += 1
+        else:
+            count[i] = 1
+    return count
